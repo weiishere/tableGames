f9a550567fc43c0f465ee24934eee4a5c958d7d2
黄伟 <wei.huang@hotmail.com>
2018-06-06 02:06:24 +0800

    修复胡牌算法

--- client/room/test.js
+++ client/room/test.js
@@ -106,9 +106,9 @@ const testWin = (cards) => {
         });
     }
     //抽出指定牌型的连子（因为从最小的牌比较的）
-    const keBijiao = (cards,index) => {
-        let copy_cards=clone(cards);
-        let frist, second,_card = copy_cards[index];
+    const keBijiao = (cards, index) => {
+        let copy_cards = clone(cards);
+        let frist, second, _card = copy_cards[index];
         frist = copy_cards.find(card => card.color === _card.color && card.number - _card.number == 1);
         if (frist) {
             second = copy_cards.find(card => card.color === frist.color && card.number - frist.number == 1);
@@ -116,12 +116,12 @@ const testWin = (cards) => {
         if (frist && second) {
             //如果有靠的，那么清除掉这三个，继续走
             copy_cards = copy_cards.filter(card => (card.key !== _card.key && card.key !== frist.key && card.key !== second.key));
-        }else{
+        } else {
             index++;
         }
-        if(copy_cards.length>index){
-            keBijiao(copy_cards,index);
-        }else{
+        if (copy_cards.length > index) {
+            return keBijiao(copy_cards, index);
+        } else {
             return copy_cards;
         }
     }
@@ -151,13 +151,13 @@ const testWin = (cards) => {
         //console.log('首先抽取的对子:' + duiziCard.number + duiziCard.color);
         //console.log(copy_cards.map(card => card.number + card.color));//抽了一个对和其他所有的克剩下的牌，做连子判断
         const tCard = copy_cards[0];
-        const remainCard = keBijiao(copy_cards,0);
-        if(remainCard.length===0){
+        const remainCard = keBijiao(copy_cards, 0);
+        if (remainCard.length === 0) {
             isWin = true;
             break;
-        }else{
+        } else {
             let { resultType_1, resultType_2 } = getCardShowTime(remainCard);
-            if(resultType_2.one.length===0&&resultType_2.two.length===0&&resultType_2.three.length!==0&&resultType_2.one.four===0){
+            if (resultType_2.one.length === 0 && resultType_2.two.length === 0 && resultType_2.three.length !== 0 && resultType_2.four.length === 0) {
                 isWin = true;
                 break;
             }
@@ -268,11 +268,11 @@ const testWin2 = (cards) => {
     return isWin;
 }
 
-// let mycards = [
-//     { key: 'w-1-1', color: 'w', number: 1 }, { key: 'w-2-1', color: 'w', number: 2 }, { key: 'w-3-1', color: 'w', number: 3 }, { key: 'w-3-2', color: 'w', number: 3 }, { key: 'w-3-3', color: 'w', number: 3 },
-//     { key: 'w-4-1', color: 'w', number: 4 }, { key: 'w-4-1', color: 'w', number: 4 }, { key: 'w-5-1', color: 'w', number: 5 }, { key: 'w-5-1', color: 'w', number: 5 }, { key: 't-7-1', color: 't', number: 7 }, { key: 't-7-1', color: 't', number: 7 },
-//     { key: 't-8-1', color: 't', number: 8 }, { key: 't-8-2', color: 't', number: 8 }, { key: 't-8-3', color: 't', number: 8 },
-// ]
+let mycards = [
+    { key: 'w-1-1', color: 'w', number: 1 }, { key: 'w-2-1', color: 'w', number: 2 }, { key: 'w-3-1', color: 'w', number: 3 }, { key: 'w-3-2', color: 'w', number: 3 }, { key: 'w-3-3', color: 'w', number: 3 },
+    { key: 'w-4-1', color: 'w', number: 4 }, { key: 'w-4-2', color: 'w', number: 4 }, { key: 'w-5-1', color: 'w', number: 5 }, { key: 'w-5-2', color: 'w', number: 5 }, { key: 't-7-1', color: 't', number: 7 }, { key: 't-7-2', color: 't', number: 7 },
+    { key: 't-8-1', color: 't', number: 8 }, { key: 't-8-2', color: 't', number: 8 }, { key: 't-8-3', color: 't', number: 8 },
+]
 
 // let mycards = [
 //     { key: 't-2-1', color: 't', number: 2 }, { key: 't-2-2', color: 't', number: 2 },
@@ -282,13 +282,13 @@ const testWin2 = (cards) => {
 //     { key: 'w-4-1', color: 'w', number: 4 }, { key: 'w-5-1', color: 'w', number: 5 }, { key: 'w-6-1', color: 'w', number: 6 }, 
 // ]
 
-let mycards = [
-    { key: 't-1-1', color: 't', number: 1 }, { key: 't-1-2', color: 't', number: 1 },
-    { key: 't-1-2', color: 't', number: 1 }, { key: 't-3-1', color: 't', number: 3 }, { key: 't-4-1', color: 't', number: 4 },
-    { key: 't-5-1', color: 't', number: 5 }, { key: 'w-4-1', color: 'w', number: 4 }, { key: 'w-5-1', color: 'w', number: 5 }, 
-    { key: 'w-6-1', color: 'w', number: 6 }, { key: 'w-6-2', color: 'w', number: 6 },{ key: 'w-6-3', color: 'w', number: 6 },
-    { key: 'w-7-1', color: 'w', number: 7 }, { key: 'w-8-1', color: 'w', number: 8 }, { key: 'w-9-1', color: 'w', number: 9 }, 
-]
+// let mycards = [
+//     { key: 't-1-1', color: 't', number: 1 }, { key: 't-1-2', color: 't', number: 1 },
+//     { key: 't-1-2', color: 't', number: 1 }, { key: 't-3-1', color: 't', number: 3 }, { key: 't-4-1', color: 't', number: 4 },
+//     { key: 't-5-1', color: 't', number: 5 }, { key: 'w-4-1', color: 'w', number: 4 }, { key: 'w-5-1', color: 'w', number: 5 }, 
+//     { key: 'w-6-1', color: 'w', number: 6 }, { key: 'w-6-2', color: 'w', number: 6 },{ key: 'w-6-3', color: 'w', number: 6 },
+//     { key: 'w-7-1', color: 'w', number: 7 }, { key: 'w-8-1', color: 'w', number: 8 }, { key: 'w-9-1', color: 'w', number: 9 }, 
+// ]
 
 debugger
 const result = testWin(mycards);
--- server/gameEngine/majiang/winCore.js
+++ server/gameEngine/majiang/winCore.js
@@ -21,6 +21,7 @@ module.exports = (cards) => {
     let twoNumber = 0;
     let threeNumber = 0;
     let fourNumber = 0;
+    let lastComparentCard;
     for (let item in group) {
         if (group[item].count === 1) singleNumber++;
         if (group[item].count === 2) twoNumber++;
@@ -47,24 +48,29 @@ module.exports = (cards) => {
         });
     }
     //抽出指定牌型的连子（因为从最小的牌比较的）
-    const keBijiao = (_card) => {
-        let frist, second;
-        frist = copy_cards.find(card => card.color === _card.color && card.number - _card.number == 1);
-        if (frist) {
-            second = copy_cards.find(card => card.color === frist.color && card.number - frist.number == 1);
-        }
-        if (frist && second) {
-            //如果有靠的，那么清除掉这三个，继续走
-            copy_cards = copy_cards.filter(card => (card.key !== frist.key && card.key !== second.key));
-            if (copy_cards.length === 0) {
-                return true;
-            } else {
-                const tCard = copy_cards.splice(0, 1)[0];
-                return keBijiao(tCard);
-            }
+    const keBijiao = (cards, index) => {
+        let copy_cards = clone(cards);
+        let frist, second, _card = copy_cards[index];
+        if (lastComparentCard && lastComparentCard.color === _card.color && lastComparentCard.number === _card.number) {
+            //跟上次比较的一样，index+1并跳过
+            index++;
         } else {
-            //不能胡牌
-            return false;
+            frist = copy_cards.find(card => card.color === _card.color && card.number - _card.number == 1);
+            if (frist) {
+                second = copy_cards.find(card => card.color === frist.color && card.number - frist.number == 1);
+            }
+            if (frist && second) {
+                //如果有靠的，那么清除掉这三个，继续走
+                copy_cards = copy_cards.filter(card => (card.key !== _card.key && card.key !== frist.key && card.key !== second.key));
+            } else {
+                index++;
+            }
+        }
+        lastComparentCard = _card;
+        if (copy_cards.length > index) {
+            return keBijiao(copy_cards, index);
+        } else {
+            return copy_cards;
         }
     }
     let lianziCard = [];
@@ -92,14 +98,15 @@ module.exports = (cards) => {
         }
         //console.log('首先抽取的对子:' + duiziCard.number + duiziCard.color);
         //console.log(copy_cards.map(card => card.number + card.color));//抽了一个对和其他所有的克剩下的牌，做连子判断
-        const tCard = copy_cards.splice(0, 1)[0];
-        const result = keBijiao(tCard);
-        if (result) {
+        const tCard = copy_cards[0];
+        lastComparentCard = null;
+        const remainCard = keBijiao(copy_cards, 0);
+        if (remainCard.length === 0) {
             isWin = true;
             break;
-        }else{
-            let { resultType_1, resultType_2 } = tool.getCardShowTime(copy_cards);
-            if(resultType_2.one.length===0&&resultType_2.two.length===0&&resultType_2.three.length!==0&&resultType_2.one.four===0){
+        } else {
+            let { resultType_1, resultType_2 } = tool.getCardShowTime(remainCard);
+            if (resultType_2.one.length === 0 && resultType_2.two.length === 0 && resultType_2.three.length !== 0 && resultType_2.four.length === 0) {
                 isWin = true;
                 break;
             }
